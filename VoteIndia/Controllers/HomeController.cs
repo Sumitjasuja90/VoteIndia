7080a1d373ea14e64d67a9a2614b99dd10df6d29
sumitjasuja90 <sumitjasuja90@gmail.com>
2017-07-25 09:27:33 +0530

    Adding XML/QuestionsAnswers.xml

--- VoteIndia/Controllers/HomeController.cs
+++ VoteIndia/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using DataObject;
+using BusinessObject;
+using VoteIndia.Models;
 namespace VoteIndia.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            QuestionsAnswer qa = new QuestionsAnswer().GetAllQuestionsFromXML();
+            return View(qa);
         }
     }
 }
---
+++ VoteIndia/Models/QuestionsAnswer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace VoteIndia.Models
+{
+    public class QuestionsAnswer
+    {
+        public int id;
+        public string Description;
+        public List<string> Options = new List<string>();
+
+        public QuestionsAnswer GetAllQuestionsFromXML()
+        {
+            XDocument doc = XDocument.Load(HttpContext.Current.Server.MapPath("~/App_Data/XML/QuestionsAnswers.xml"));
+            XElement questions = doc.Element("questions");
+            QuestionsAnswer result = new QuestionsAnswer();
+            foreach (XElement qa in questions.Elements("question"))
+            {
+                if (qa.Attribute("id").Value == "1")
+                {
+                    result.Description = qa.Element("description").Value;
+                    result.Options.Add(qa.Element("options").Element("a").Value);
+                    result.Options.Add(qa.Element("options").Element("b").Value);
+                    result.Options.Add(qa.Element("options").Element("c").Value);
+                    result.Options.Add(qa.Element("options").Element("d").Value);
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
